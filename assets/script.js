adb1705e6cea9ecbd99c7179c164b5c29b90c89c
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-06-09 20:05:08 -0400

    Added styling on header

--- assets/script.js
+++ assets/script.js
@@ -4,26 +4,50 @@ function moveToSection(section) {
         portfolio: document.querySelector("#portfolio"),
         contact: document.querySelector("#contact-me"),
     };
-    console.log(section)
-    Object.keys(sections).forEach(function (key) {
 
-        if (section === key && sections[key].classList.contains("d-none")) {
-            sections[key].classList.remove("d-none");
+    Object.keys(sections).forEach(function (key) {
+        if (section === key) {
+            if (sections[key].classList.contains("d-none")) {
+                sections[key].classList.remove("d-none");
+            }
         } else if (section != key && !sections[key].classList.contains("d-none")) {
             sections[key].classList.add("d-none");
         }
     });
 
-    setTimeout(function(){
-        sections[section].scrollIntoView(true, { behavior: "smooth" })
-    }, 300)
-
+    // setTimeout(function () {
+    //     sections[section].scrollIntoView(true, { behavior: "smooth" });
+    // }, 400);
 }
 
+function toggleButtons(buttonI) {
+    const buttonList = [
+        document.querySelector("#about-button"),
+        document.querySelector("#portfolio-button"),
+        document.querySelector("#contact-button"),
+    ];
+    buttonList.forEach(function (button, index) {
+        if (index === buttonI) {
+            if (!button.classList.contains("active")) {
+                button.classList.add("active");
+            }
+        } else {
+            button.classList.remove("active");
+        }
+    });
+}
+
+document.querySelector("#about-button").addEventListener("click", function (event) {
+    moveToSection("about");
+    toggleButtons(0);
+});
+
 document.querySelector("#portfolio-button").addEventListener("click", function (event) {
-    moveToSection('portfolio')
+    moveToSection("portfolio");
+    toggleButtons(1);
 });
 
 document.querySelector("#contact-button").addEventListener("click", function (event) {
-    moveToSection('contact')
+    moveToSection("contact");
+    toggleButtons(2);
 });
