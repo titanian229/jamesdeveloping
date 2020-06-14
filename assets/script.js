bb0497debfd569e44c7c806f9de4341cf130d9b5
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-06-14 14:17:02 -0400

    Removed bootstrap navbar, added google fonts for testing

--- assets/script.js
+++ assets/script.js
@@ -8,24 +8,58 @@ function moveToSection(section) {
     Object.keys(sections).forEach(function (key) {
         if (section === key) {
             if (sections[key].classList.contains("d-none")) {
-                setTimeout(function(){
-                sections[key].classList.remove("d-none");
-                sections[key].classList.remove("animate__animated", "animate__fadeOut", "animate__faster");
-                sections[key].classList.add("animate__animated", "animate__fadeIn", "animate__faster");
-                }, 500)
+                setTimeout(function () {
+                    sections[key].classList.remove("d-none");
+                    sections[key].classList.remove(
+                        "animate__animated",
+                        "animate__fadeOut",
+                        "animate__faster"
+                    );
+                    sections[key].classList.add(
+                        "animate__animated",
+                        "animate__fadeIn",
+                        "animate__faster"
+                    );
+                }, 500);
             }
         } else if (section != key && !sections[key].classList.contains("d-none")) {
-            sections[key].classList.remove("animate__animated", "animate__fadeIn", "animate__faster");
+            sections[key].classList.remove(
+                "animate__animated",
+                "animate__fadeIn",
+                "animate__faster"
+            );
             sections[key].classList.add("animate__animated", "animate__fadeOut", "animate__faster");
             setTimeout(function () {
                 sections[key].classList.add("d-none");
             }, 500);
         }
     });
-
     // setTimeout(function () {
     //     sections[section].scrollIntoView(true, { behavior: "smooth" });
     // }, 400);
+}
+var fontI = 0;
+const fonts = [
+    "Bellota",
+    "Source Code Pro",
+    "Comfortaa",
+    "Kalam",
+    "Lobster Two",
+    "Poiret One",
+    "Handlee",
+    "Press Start 2P",
+    "Monoton",
+    "IBM Plex Mono",
+    "Roboto",
+    "Open Sans",
+    "Noto Sans JP",
+    "Noto Sans HK",
+    "Roboto",
+];
+function cycleFont() {
+    fontI = (fontI + 1) % fonts.length
+    document.querySelector("body").style.fontFamily = fonts[fontI];
+    console.log(fonts[fontI])
 }
 
 function toggleButtons(buttonI) {
@@ -58,4 +92,8 @@ document.querySelector("#portfolio-button").addEventListener("click", function (
 document.querySelector("#contact-button").addEventListener("click", function (event) {
     moveToSection("contact");
     toggleButtons(2);
+});
+
+document.querySelector("#hamburger").addEventListener("click", function () {
+    document.querySelector("#nav").classList.toggle("respond");
 });
